a2a02ed0af5ea012f1e1472541927712a888599b
Nehemiah <nehemiah.moses@gmail.com>
2015-06-20 17:59:38 +0800

    remove sample data

--- www/js/app.js
+++ www/js/app.js
@@ -6,12 +6,7 @@
 angular.module('todo', ['ionic'])
 
 .controller('TodoCtrl', function($scope) {
-	$scope.tasks = [
-		{ title: 'Collect coins' },
-	    { title: 'Eat mushrooms' },
-    	{ title: 'Get high enough to grab the flag' },
-    	{ title: 'Find the Princess' }
-  	];
+	$scope.tasks = [];
 })
 
 .run(function($ionicPlatform) {
