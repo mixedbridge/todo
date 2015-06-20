f0d5c8f411cd339e9ff08d4638cb45440f448ef3
Nehemiah <nehemiah.moses@gmail.com>
2015-06-20 14:20:09 +0800

    Add JS controller

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,15 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('todo', ['ionic'])
 
+.controller('TodoCtrl', function($scope) {
+	$scope.tasks = [
+		{ title: 'Collect coins' },
+	    { title: 'Eat mushrooms' },
+    	{ title: 'Get high enough to grab the flag' },
+    	{ title: 'Find the Princess' }
+  	];
+});
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
